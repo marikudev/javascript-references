a2f381de6f4563ae6dbed0b7815ec19a1a0c1864
marikudev <malik.workfile@gmail.com>
2025-05-11 06:20:07 +0900

    remove whitespace from the end of a string

---
+++ trim.js
@@ -0,0 +1,6 @@
+// trim menghapus spasi atau whitespace (seperti spasi, tab, dan newline) dari awal dan akhir sebuah string.
+
+const teks = "Mohamad Faizal Malik ";
+const spaceTeks = teks.trim(); 
+
+console.log(spaceTeks);
