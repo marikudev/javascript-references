c7206b4ced2ba52cf3ae2d261b51c1681a3d22b4
marikudev <malik.workfile@gmail.com>
2025-05-11 07:58:34 +0900

    add test indexOf data

---
+++ indexOf.js
@@ -0,0 +1,10 @@
+const message = "JavaScript is not Java";
+
+const index = message.indexOf("ot");
+console.log("indeks ke: " + index);
+
+
+let str = "JavaScriptaap cnp"
+
+let startindex = str.indexOf("p", 2);
+console.log(startindex);
