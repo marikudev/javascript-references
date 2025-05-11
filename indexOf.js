a2b498c21f4b3d04853acd0fae130dc5eebca4e8
marikudev <malik.workfile@gmail.com>
2025-05-12 08:36:21 +0900

    add other example (use .push to add array)

--- indexOf.js
+++ indexOf.js
@@ -4,7 +4,35 @@ const index = message.indexOf("ot");
 console.log("indeks ke: " + index);
 
 
-let str = "JavaScriptaap cnp"
+// Argumen kedua menentukan indeks awal pencarian
 
-let startindex = str.indexOf("p", 2);
-console.log(startindex);
+let index2 = message.indexOf("a", 2);
+console.log(index2);
+
+// indexOf returns -1 if not found
+
+let index3 = message.indexOf("Phyton");
+console.log(index3);
+
+// Finding All the Occurrences (kemunculan/keberadaan) of an Element
+
+function findAllIndex(string, value) {
+    indices = [];
+    var currentIndex = string.indexOf(value);
+    while (currentIndex != -1) {
+      indices.push(currentIndex);
+      currentIndex = string.indexOf(value, currentIndex + value.length);
+    }
+    return indices;
+  }
+  
+  var str = "JavaScript is as related to Java as Carpenter is to Carpet.";
+  
+  var occurance1 = findAllIndex(str, "J");
+  console.log(occurance1); // [ 0, 28 ]
+  
+  var occurance2 = findAllIndex(str, "Carpet");
+  console.log(occurance2); // [ 52 ]
+  
+  var occurance3 = findAllIndex(str, "x");
+  console.log(occurance3); // []
