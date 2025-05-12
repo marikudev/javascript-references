9df88eb3fff8216cd7a7874896e8809e59f2042c
marikudev <malik.workfile@gmail.com>
2025-05-13 05:40:25 +0900

    add toLowerCase - string

---
+++ string/toLowerCase.js
@@ -0,0 +1,11 @@
+//-- toLowerCase() Syntax --
+//-- string.toLowerCase() --
+
+let str = "HELLO WORLD!";
+let sentence = "Java is to JavaScript what Car is to Carpet.";
+
+let lowercase_str = str.toLowerCase();
+console.log(lowercase_str); // hello world!
+
+let lowercase = sentence.toLowerCase();
+console.log(lowercase); // java is to javascript what car is to carpet.
