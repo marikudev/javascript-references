a7866907812fec318d9966b4e81db5b5053c1fda
marikudev <malik.workfile@gmail.com>
2025-05-21 18:34:50 +0900

    update data

--- string/length.js
+++ string/length.js
@@ -1,5 +1,19 @@
+//-> length returns the number of characters in a string.
+//-- Syntax: string.length
+
 let sentence = "I love Programiz.";
 
 let len = sentence.length;
 
-console.log(len);
+console.log(len); // 17
+
+let string2 = "Programming";
+
+// assigning a value to string's length property
+string2.length = 5;
+
+// doesn't change the original string
+console.log(string2); // Programming
+
+// returns the length of 'Programming'
+console.log(string2.length); // 11
